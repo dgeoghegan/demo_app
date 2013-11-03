5c3a91ea6e9db0221dbc04b1ece59b8b7165dce4
Denis Geoghegan <denis_geoghegan@yahoo.com>
2013-11-03 00:27:14 -0400

    finish demo app

--- Gemfile
+++ Gemfile
@@ -16,3 +16,7 @@ group :assets do
 end
 
 gem 'jquery-rails'
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ app/models/micropost.rb
@@ -0,0 +1,6 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :contents, :user_id
+  belongs_to :user
+
+  validates :contents, :length => { :maximum => 140 }
+end
